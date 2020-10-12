dc17b35af9901c83976eec728d9a44c51273ed62
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-12 13:11:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,22 @@
 # Write your code here!
+
+#contains and returns a hash 
+  
+  #top level has 2 keys: :home and :away.
+  #values of top keys are hashes.
+  
+  #:team_name #> "'team name'"
+  
+  #:colors #> ["color1", "color2", "color3"]
+  
+  #:players #> [{:player_name #> ,:number #> ,:shoe #> ,:points #> ,:rebounds #> ,:assists #> , :steals #> , :blocks #> , : slam_dunks #>}, {}, {}]
+
+
+def game_hash 
+  
+  hash = {
+    :home => {:team_name => "Brooklyn Nets", :colors =>} , 
+    :away  => {}
+  }
+  
+end
