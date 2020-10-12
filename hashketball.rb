dfc7b44713e8de622ec533fb6bb6d2c2c8d48ea0
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-12 13:38:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,6 +75,36 @@ def game_hash
       :team_name => "Charlotte Hornets", 
       :colors => ["Turquoise", "Purple"],
       :players => [{
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      },{
+        :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      } ,{
+        :player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => 
+      } ,{
         :player_name => ,
         :number => ,
         :shoe => ,
@@ -84,7 +114,17 @@ def game_hash
         :steals => ,
         :blocks => ,
         :slam_dunks => 
-      },
+      } ,{
+        :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => 
+      } 
     }
   } 
   
